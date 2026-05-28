f71302027b800be9b16d3bac1d890d3beb2aba11
Mradul Pal <mradulpal@outlook.com>
2026-05-28 20:44:28 +0530

    JFrame Completed

--- src/MatchCards.java
+++ src/MatchCards.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 import java.util.ArrayList;
 
@@ -33,13 +35,71 @@ public class MatchCards {
     JPanel boardPanel = new JPanel();
     JPanel restartGamePanel = new JPanel();
     JButton restartButton = new JButton();
+    ImageIcon CardBackImageIcon;
 
     int errorCount = 0;
     ArrayList<JButton> board;
+    Timer hideCardTimer;
+    boolean gameReady = false;
+    JButton card1Selected;
+    JButton card2Selected;
 
     MatchCards() {
         setupCards();
         shuffleCards();
+
+        frame.setLayout(new BorderLayout());
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Errors: " + Integer.toString(errorCount));
+        textLabel.setPreferredSize(new Dimension(boardWidth, 30));
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        board = new ArrayList<JButton>();
+        for (int i = 0; i < cardSet.size(); i++) {
+            JButton tile = new JButton();
+            tile.setPreferredSize(new Dimension(cardWidth, cardHeight));
+            tile.setOpaque(true);
+            tile.setIcon(cardSet.get(i).cardImageIcon);
+            tile.setFocusable(false);
+            tile.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (!gameReady) {
+                        return;
+                    }
+                    JButton tile = (JButton) e.getSource();
+                    if (tile.getIcon() == cardBackImageIcon) {
+                        if (card1Selected == null) {
+                            card1Selected = tile;
+                            int index = board.indexOf(card1Selected);
+                            card1Selected.setIcon(cardSet.get(index).cardImageIcon);
+                        } else if (card2Selected == null) {
+                            card2Selected = tile;
+                            int index = board.indexOf(card2Selected);
+                            card2Selected.setIcon(cardSet.get(index).cardImageIcon);
+                            if (card1Selected.getIcon() != card2Selected.getIcon()) {
+                                errorCount++;
+                                textLabel.setText("Errors: " + Integer.toString(errorCount));
+                                hideCardTimer.start();
+                            } else {
+                                card1Selected = null;
+                                card2Selected = null;
+                            }
+                        }
+                    }
+                }
+            });
+            board.add(tile);
+            boardPanel.add(tile);
+        }
+        frame.add(boardPanel);
     }
 
     void setupCards() {
@@ -47,6 +107,21 @@ public class MatchCards {
         for (String cardName : cardList) {
             Image cardImg = new ImageIcon(getClass().getResource("./img/" + cardName + ".jpg")).getImage();
             ImageIcon cardImageIcon = new ImageIcon(cardImg.getScaledInstance(cardWidth, cardHeight, Image.SCALE_SMOOTH));
+            Card card = new Card(cardName, cardImageIcon);
+            cardSet.add(card);
+        }
+        cardSet.addAll(cardSet);
+
+        Image cardBackImg = new ImageIcon(getClass().getResource("./img/back.jpg")).getImage();
+        cardBackImageIcon = new ImageIcon(cardBackImg.getScaledInstance(cardWidth, cardHeight, Image.SCALE_SMOOTH));
+    }
+
+    void shuffleCards() {
+        for (int i = 0; i < cardSet.size(); i++) {
+            int j = (int)(Math.random() * cardSet.size());
+            Card temp = cardSet.get(i);
+            cardSet.set(i, cardSet.get(j));
+            cardSet.set(j, temp);
         }
     }
 }
